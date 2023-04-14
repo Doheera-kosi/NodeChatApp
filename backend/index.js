4d248e6592ecc875c3e75e437ea7f2161f73f5c0
Doheera-kosi <kupourevans99@gmail.com>
2023-04-14 21:33:00 +0000

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,3 +4,8 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+    const { username } =req.body;
+    return res.json({ username: username, secret: "sha256..." });
+});
