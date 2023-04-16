05f3185ed1fac954421562bd5fd520c351f4faf9
Doheera-kosi <kupourevans99@gmail.com>
2023-04-16 06:38:18 +0000

    Modify index.js

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -7,6 +8,18 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
     const { username } =req.body;
+
+    try {
+        const r = await axios.put(
+            'https://api.chatengine.io/users/',
+            {username: username, secret: username, first_name: username},
+            {headers: {"private-key": "8c63dbce-80a7-455a-890b-9368d16e1dcb"}}
+        )
+        return res.status(r.status).json(r.data)
+    } catch (e) {
+        
+    }
+
     return res.json({ username: username, secret: "sha256..." });
 });
 
