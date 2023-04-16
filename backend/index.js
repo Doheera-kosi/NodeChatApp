c74d64f5896e48f665e963c45ce4f85d3fc4787c
Doheera-kosi <kupourevans99@gmail.com>
2023-04-16 06:41:02 +0000

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.post("/authenticate", async (req, res) => {
         )
         return res.status(r.status).json(r.data)
     } catch (e) {
-        
+        return res.status(e.response.status).json(e.response.data)
     }
 
     return res.json({ username: username, secret: "sha256..." });
