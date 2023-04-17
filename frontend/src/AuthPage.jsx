a8c08bdd1b0e045cc461887eccab2594630e5240
Doheera-kosi <kupourevans99@gmail.com>
2023-04-17 12:19:16 +0000

    Update AuthPage.jsx

--- frontend/src/AuthPage.jsx
+++ frontend/src/AuthPage.jsx
@@ -1,27 +1,34 @@
+// import React from 'react';
+import PropTypes from 'prop-types';
+
 const AuthPage = (props) => {
-    const onSubmit = (e) => {
-      e.preventDefault();
-      const { value } = e.target[0];
-      props.onAuth({ username: value, secret: value });
-    };
-  
-    return (
-      <div className="background">
-        <form onSubmit={onSubmit} className="form-card">
-          <div className="form-title">Welcome 👋</div>
-  
-          <div className="form-subtitle">Set a username to get started</div>
-  
-          <div className="auth">
-            <div className="auth-label">Username</div>
-            <input className="auth-input" name="username" />
-            <button className="auth-button" type="submit">
-              Enter
-            </button>
-          </div>
-        </form>
-      </div>
-    );
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const { value } = e.target[0];
+    props.onAuth({ username: value, secret: value });
+  };
+
+  return (
+    <div className="background">
+      <form onSubmit={onSubmit} className="form-card">
+        <div className="form-title">Welcome 👋</div>
+
+        <div className="form-subtitle">Set a username to get started</div>
+
+        <div className="auth">
+          <div className="auth-label">Username</div>
+          <input className="auth-input" name="username" />
+          <button className="auth-button" type="submit">
+            Enter
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
-  
+
+AuthPage.propTypes = {
+  onAuth: PropTypes.func.isRequired,
+};
+
 export default AuthPage;
