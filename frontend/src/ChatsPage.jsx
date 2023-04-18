1f775368214b121e356f31c4d577818481d354f9
Doheera-kosi <kupourevans99@gmail.com>
2023-04-18 14:05:06 +0000

    Update UI with pretty

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,17 +1,43 @@
+// // import React from "react";
+// import PropTypes from "prop-types";
+// import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced';
+
+// const ChatsPage = (props) => {
+//     const ChatProps = useMultiChatLogic(
+//         '98f642b4-b1d1-4db4-be1d-7b1d0dfd2229',
+//         props.user.username,
+//         props.user.secret
+//     );
+//     return (
+//         <div style={{height: '100vh' } }>
+//             <MultiChatSocket {...ChatProps} />
+//             <MultiChatWindow {...ChatProps} style={{height: '100vh' }}/>
+//         </div>
+//     )
+// }
+
+// ChatsPage.propTypes = {
+//   user: PropTypes.shape({
+//     username: PropTypes.string.isRequired,
+//     secret: PropTypes.string.isRequired
+//   }).isRequired
+// };
+
+// export default ChatsPage;
+
 // import React from "react";
 import PropTypes from "prop-types";
-import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced';
+import { PrettyChatWindow} from 'react-chat-engine-pretty';
 
 const ChatsPage = (props) => {
-    const ChatProps = useMultiChatLogic(
-        '98f642b4-b1d1-4db4-be1d-7b1d0dfd2229',
-        props.user.username,
-        props.user.secret
-    );
     return (
         <div style={{height: '100vh' } }>
-            <MultiChatSocket {...ChatProps} />
-            <MultiChatWindow {...ChatProps} style={{height: '100vh' }}/>
+            <PrettyChatWindow
+                projectId="98f642b4-b1d1-4db4-be1d-7b1d0dfd2229"
+                username={props.user.username}
+                secret={props.user.secret}
+                style={{height: '100%'}}
+            />
         </div>
     )
 }
