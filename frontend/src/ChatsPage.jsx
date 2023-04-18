8ba876e3672aafcb7e4b7c5b93f131aa1e6ab2ba
Doheera-kosi <kupourevans99@gmail.com>
2023-04-18 13:47:30 +0000

    Modify ChatsPage.jsx

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,21 @@
-const ChatsPage = () => {
-    return<>Chats...!</>
+import {
+    MultiChatSocket,
+    MultiChatWindow,
+    useMultiChatLogic
+} from 'react-chat-engine-advanced'
+
+const ChatsPage = (props) => {
+    const ChatProps = useMultiChatLogic(
+        '98f642b4-b1d1-4db4-be1d-7b1d0dfd2229',
+        props.user.username,
+        props.user.secret
+    );
+    return (
+        <div style={{height: '100vh' } }>
+            <MultiChatSocket {...ChatProps} />
+            <MultiChatWindow {...ChatProps} style={{height: '100vh' }}/>
+        </div>
+    )
 }
 
 export default ChatsPage;
